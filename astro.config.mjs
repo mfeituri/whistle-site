cf227ae676551355b00a479c9a6c0ad28736cc34
Mohamed Feituri <mohamedfeituri@Mohameds-MacBook-Pro-7.local>
2025-04-30 01:43:35 -0600

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'static',
   base: '/whistle-site/',
-  integrations: [tailwind()], 
+  output: 'static',
+  integrations: [tailwind()],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
